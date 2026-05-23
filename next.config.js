48465b82378e39be9dea1b9898072ce460cc3323
TheWeirdDee <divinenation1@gmail.com>
2026-05-23 13:06:57 +0100

    fix: Transpile Stacks packages to fix production build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  transpilePackages: ['@stacks/connect', '@stacks/connect-react', '@stacks/network', '@stacks/transactions', '@stacks/auth', '@stacks/profile'],
 };
 module.exports = nextConfig;

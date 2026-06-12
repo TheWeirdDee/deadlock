1f87b7076768e90f6633c9845f15937e3fb343b7
TheWeirdDee <divinenation1@gmail.com>
2026-06-12 14:34:46 +0100

    add @deadlock/sdk public index exports

---
+++ sdk/src/index.ts
@@ -0,0 +1,4 @@
+export { DeadlockClient } from './client';
+export type { DeadlockClientConfig } from './types';
+export { VowType, VowStatus } from './types';
+export type { Vow, SpectatorPool, SpectatorBet } from './types';
